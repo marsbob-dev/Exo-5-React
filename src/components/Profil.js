2119e4042cd5dec326e3518f4f832fda6ddcfacc
Marcelin <marsbob@live.fr>
2021-01-14 13:00:46 +0100

    Création du component profil et importation dans app

---
+++ src/components/Profil.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Profil extends Component {
+    render(){
+        return (
+            <div>
+                <h1>Bohbot</h1>
+                <h2>Marcelin</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, quam.</p>
+            </div>
+        )
+    }
+}
+
+export default Profil
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Profil from './components/Profil'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Profil />
+    <Profil />
+    <Profil />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
